e901df6da896538ddd3cca46a00d217a2d4b6be6
Angelo Carly <angelo.carly@protonmail.com>
2025-08-10 14:45:22 +0200

    Load a png

--- src/editor.rs
+++ src/editor.rs
@@ -1,34 +1,41 @@
 use ash::vk;
-use ash::vk::{AccessFlags, DescriptorSet, ImageLayout, ImageUsageFlags, ImageView, PipelineStageFlags, Sampler};
+use ash::vk::{AccessFlags, BufferImageCopy, BufferUsageFlags, DescriptorSet, DeviceSize, ImageLayout, ImageUsageFlags, ImageView, PipelineStageFlags, Sampler};
 use cen::app::gui::{GuiComponent, GuiSystem};
 use cen::graphics::Renderer;
 use cen::graphics::renderer::RenderComponent;
-use cen::vulkan::{CommandBuffer, DescriptorSetLayout, Image};
-use egui::{ImageSource, TextureId, Vec2, Widget};
+use cen::vulkan::{Buffer, CommandBuffer, DescriptorSetLayout, Image};
+use egui::{ImageSize, ImageSource, Pos2, Rect, Scene, TextureId, Vec2, Widget};
 use egui::load::SizedTexture;
 use egui_dock::{DockArea, DockState, NodeIndex, Style};
+use gpu_allocator::MemoryLocation;
+use image::{EncodableLayout, GenericImageView};
 
 pub struct Editor {
     pub tree: DockState<String>,
     image: Option<Image>,
     texture_id: Option<TextureId>,
+    tab_viewer: Option<TabViewer>,
 }
 
 impl Editor {
     pub(crate) fn new() -> Self {
 
-        let mut tree = DockState::new(vec!["tab1".to_owned(), "tab2".to_owned()]);
+        let mut tree = DockState::new(vec!["view".to_owned(), "extra".to_owned()]);
 
         let [a, b] =
             tree.main_surface_mut()
-                .split_left(NodeIndex::root(), 0.3, vec!["tab3".to_owned()]);
+                .split_left(NodeIndex::root(), 0.3, vec!["tools".to_owned()]);
 
-        Self { tree, texture_id: None, image: None }
+        Self { tree, texture_id: None, image: None,
+            tab_viewer: None
+        }
     }
 }
 
 struct TabViewer {
     texture_id: TextureId,
+    texture_size: Vec2,
+    scene_rect: Rect
 }
 
 impl egui_dock::TabViewer for TabViewer {
@@ -39,14 +46,29 @@ impl egui_dock::TabViewer for TabViewer {
     }
 
     fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
-        ui.label(format!("Content of {tab}"));
-        if tab == "tab1" {
-            egui::Image::new(ImageSource::Texture(SizedTexture {
-                id: self.texture_id,
-                size: Vec2 { x: 100., y: 100. }
-            })).ui(ui);
+        if tab == "view" {
+            egui::Frame::group(ui.style())
+                .inner_margin(0.0)
+                .show(ui, |ui| {
+                    let scene = Scene::new()
+                        .max_inner_size([350.0, 1000.0])
+                        .zoom_range(0.1..=30.0);
 
-            ui.label(format!("Title: {tab}"));
+                    let mut inner_rect = Rect::NAN;
+                    let response = scene
+                        .show(ui, &mut self.scene_rect, |ui| {
+                            egui::Image::new(ImageSource::Texture(SizedTexture {
+                                id: self.texture_id,
+                                size: self.texture_size
+                            })).ui(ui);
+                            inner_rect = ui.min_rect();
+                        })
+                        .response;
+
+                    if response.double_clicked() {
+                        self.scene_rect = inner_rect;
+                    }
+                });
         }
     }
 }
@@ -58,23 +80,44 @@ impl GuiComponent for Editor {
             assert!(self.image.is_some());
             self.texture_id = Some(gui.create_texture(self.image.as_ref().unwrap()));
         }
+
+        self.tab_viewer = Some(TabViewer {  texture_id: self.texture_id.unwrap(), scene_rect: Rect::ZERO, texture_size: Vec2::new(self.image.as_ref().unwrap().width as f32, self.image.as_ref().unwrap().height as f32) });
     }
 
     fn gui(&mut self, gui: &GuiSystem, context: &egui::Context) {
 
         DockArea::new(&mut self.tree)
             .style(Style::from_egui(context.style().as_ref()))
-            .show(context, &mut TabViewer { texture_id: self.texture_id.unwrap() });
+            .show(context, self.tab_viewer.as_mut().unwrap());
     }
 }
 
 impl RenderComponent for Editor {
     fn initialize(&mut self, renderer: &mut Renderer) {
+
+        // Load image from disk
+        let im = image::open("./solstice.png").expect("Couldn't load image").to_rgba8();
+        let width = im.width();
+        let height = im.height();
+
+        // Load image into buffer
+        let mut buf = Buffer::new(
+            &renderer.device,
+            &mut renderer.allocator,
+            MemoryLocation::CpuToGpu,
+            (width * height * 4) as DeviceSize,
+            BufferUsageFlags::TRANSFER_SRC | BufferUsageFlags::TRANSFER_DST
+        );
+
+        let map = buf.mapped();
+        let pixel_data = im.as_bytes();
+        unsafe { std::ptr::copy_nonoverlapping(pixel_data.as_ptr(), map.as_mut_ptr(), pixel_data.len()); }
+
         self.image = Some(Image::new(
             &renderer.device,
             &mut renderer.allocator,
-            100,
-            100,
+            im.width(),
+            im.height(),
             ImageUsageFlags::STORAGE | ImageUsageFlags::SAMPLED | ImageUsageFlags::TRANSFER_DST
         ));
 
@@ -90,7 +133,26 @@ impl RenderComponent for Editor {
             AccessFlags::TRANSFER_READ,
             AccessFlags::TRANSFER_WRITE,
         );
-        command_buffer.clear_color_image(&self.image.as_ref().unwrap(), ImageLayout::TRANSFER_DST_OPTIMAL, [1., 1., 0., 1.]);
+        let regions = [
+            BufferImageCopy::default()
+                .buffer_offset(0)
+                .buffer_row_length(0)
+                .buffer_image_height(height)
+                .image_subresource(vk::ImageSubresourceLayers {
+                    aspect_mask: vk::ImageAspectFlags::COLOR,
+                    mip_level: 0,
+                    base_array_layer: 0,
+                    layer_count: 1,
+                })
+                .image_offset(vk::Offset3D { x: 0, y: 0, z: 0 })
+                .image_extent(vk::Extent3D { width, height, depth: 1 })
+        ];
+        command_buffer.copy_buffer_to_image(
+            &buf,
+            self.image.as_ref().unwrap(),
+            ImageLayout::TRANSFER_DST_OPTIMAL,
+            &regions
+        );
         renderer.transition_image(
             &command_buffer,
             self.image.as_ref().unwrap().handle(),
